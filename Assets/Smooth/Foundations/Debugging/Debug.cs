cc6941c0efe9b7b8bbd36495d910592890d5242c
tlgkccampbell <webmaster@twistedlogik.net>
2014-05-06 22:01:05 -0400

    Cleaned up logging methods

--- Assets/Smooth/Foundations/Debugging/Debug.cs
+++ Assets/Smooth/Foundations/Debugging/Debug.cs
@@ -1,30 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Smooth.Debugging {
     /// <summary>
     /// Contains methods for writing to the application's log.
     /// </summary>
     public static class Debug {
-
-        public static void LogError(Exception error) {
+        public static void LogError(Object message) {
 #if STANDARD_RUNTIME
-            System.Diagnostics.Debug.Write(error);
+            System.Diagnostics.Debug.WriteLine(message);
 #else
             UnityEngine.Debug.LogError(error);				
 #endif
         }
 
-        public static void LogError(String error) {
+        public static void LogWarning(Object message) {
 #if STANDARD_RUNTIME
-            System.Diagnostics.Debug.Write(error);
-#else
-            UnityEngine.Debug.LogError(error);				
-#endif
-        }
-
-        public static void LogWarning(String warning) {
-#if STANDARD_RUNTIME
-            System.Diagnostics.Debug.Write(warning);
+            System.Diagnostics.Debug.WriteLine(message);
 #else
             UnityEngine.Debug.LogWarning(warning);				
 #endif
